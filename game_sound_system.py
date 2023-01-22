b458a0629a5322f3bdd11b38997f2439772faf53
Giovanni Venturi <no-reply>
2023-01-22 02:10:41 +0100

    removed debug stuffs

--- game_sound_system.py
+++ game_sound_system.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import os
 
-import time
-
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 
@@ -47,12 +45,8 @@ class GameSoundSystem:
     def play(self, sound_id, fade_out_seconds = ''):
         if self.sound_system:
             if sound_id:
-                print(f"sound_id: {sound_id}")
                 if self.sound_filename[sound_id]:
                     pygame.mixer.music.load(self.sound_filename[sound_id])
                     pygame.mixer.music.play()
                     if fade_out_seconds:
                         pygame.mixer.music.fadeout(fade_out_seconds * 1000)
-            else:
-                print(f"sound_id: empty")
-            time.sleep(10)
